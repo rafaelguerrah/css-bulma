ec83ab2323af61a93a66a37fec88e1653dcf8be4
Rafael Guerra <rafael_63_@hotmail.com>
2025-03-03 11:06:42 -0300

    mudei script

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ function entrar(event) {
     const email = document.querySelector("#email").value;
     const senha =(document.querySelector("#senha")).value;
 
-    if (email === "login@gmail.com" && senha === "1234"){
-        window.location.href ="index2.html";
-    }else {
-        alert("Login errado")
+    if (email === "" || senha === "") {
+        alert("Por favor, preencha todos os campos");
+    } else if (email === "rafael_63_@hotmail.com" && senha === "1234") {
+        window.location.href = "index2.html"; 
+    } else {
+        alert("Login errado");
     }
 }
